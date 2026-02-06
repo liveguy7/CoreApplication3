729b13fb72d060f22792ab42b119a318f12a8196
jerom <jerom@LAPTOP-L0BU5SNG>
2026-02-05 19:04:03 -0500

    changeInit

--- CoreApplication3/Startup.cs
+++ CoreApplication3/Startup.cs
@@ -19,7 +19,7 @@ namespace CoreApplication3
             app.Run(async (context) =>
             {
                 await context.Response
-                    .WriteAsync("Hello World");
+                    .WriteAsync("Jello");
             });
         }
     }
